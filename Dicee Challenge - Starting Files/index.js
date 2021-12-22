f29e174c301a8de0883d19f2c3e16cc0eea24861
utkarsh-e4 <utkarshsns@gmail.com>
2021-12-22 09:50:37 +0530

    First commit

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,23 @@
+var randomNumber=Math.floor(Math.random()*6 + 1);
+var randomImage="dice" + randomNumber + ".png";
+var randomImageSrc="images/" + randomImage;
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSrc);
+
+var randomNumber2=Math.floor(Math.random()*6 + 1);
+var randomImage2="dice" + randomNumber2 + ".png";
+var randomImageSrc2="images/" + randomImage2;
+var image2=document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImageSrc2);
+
+if(randomNumber>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins !"
+}
+else if(randomNumber2>randomNumber){
+    document.querySelector("h1").innerHTML="🚩 Player 2 Wins !"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw !"
+}
